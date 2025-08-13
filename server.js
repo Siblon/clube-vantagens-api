7ce8f5f5a7cc189942676c68145b1484f3d4e3f2
Siblon <mtf.milton.tavares@gmail.com>
2025-08-13 01:31:04 -0300

    chore: validate environment variables

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-require('dotenv').config();
+require('./config/env');
 
 const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
