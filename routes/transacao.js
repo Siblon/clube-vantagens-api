b6820c2078a8fd16dbaf6b9a775f88bf444bd7ed
Siblon <mtf.milton.tavares@gmail.com>
2025-08-13 02:38:08 -0300

    chore: remove unused route modules

--- routes/transacao.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const ctrl = require('../controllers/transacaoController');
-
-router.get('/preview', ctrl.preview);
-router.post('/', ctrl.criar);
-
-module.exports = router;
